bee0a7e31077b7b5c0c7f7b9b46d53ab260edbf4
aarnas <arnas.andriuskevicius@gmail.com>
2022-05-14 11:45:52 +0300

    Model input changed

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -204,15 +204,6 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/model.json',
-          to: path.join(__dirname, 'build'),
-          force: true,
-        },
-      ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
           from: 'src/assets/grouping/id_to_country.js',
           to: path.join(__dirname, 'build'),
           force: true,
